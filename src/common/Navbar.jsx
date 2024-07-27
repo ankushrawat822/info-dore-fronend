d853b894f268fb28a478f93623ed7ce85ea4aca2
Ankush Rawat <ankushrawat037@gmail.com>
2024-07-28 04:36:09 +0530

    update

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -3,10 +3,18 @@ import {
     Navbar as MaterialNavbar,
     Collapse,
     Typography,
-    Button,
+   
     IconButton,
     Card,
+    Button,
+    Dialog,
+    DialogHeader,
+    DialogBody,
+    DialogFooter,
+    Input,
+    Textarea,
 } from "@material-tailwind/react";
+
 import { Link } from 'react-router-dom';
 
 import { MdOutlineAccountCircle } from "react-icons/md";
@@ -18,6 +26,12 @@ import { app } from '../Firebase';
 
 
 const Navbar = () => {
+
+    const [open, setOpen] = React.useState(false);
+ 
+    const handleOpen = () => setOpen(!open);
+
+    // nav
 
     const [openNav, setOpenNav] = useState(false);
 
@@ -68,6 +82,36 @@ const Navbar = () => {
                 </Typography>
 
             </Link>
+         
+
+            <Typography
+                    onClick={handleOpen} 
+                    as="li"
+                    variant="small"
+                    color="blue-gray"
+                    className="p-1 font-normal"
+                >
+                    <p className="flex items-center">
+                        Feedback
+                    </p>
+                </Typography>
+
+
+            <Link to="/view-feedback" >
+            
+               <Typography
+                     as="li"
+                     variant="small"
+                     color="blue-gray"
+                     className="p-1 font-normal"
+                 >
+                     <p className="flex items-center">
+                         view Feedback
+                     </p>
+                 </Typography>
+
+            </Link>
+               
 
         </ul>
     );
@@ -269,6 +313,52 @@ const Navbar = () => {
                         </div>
                     </Collapse>
                 </MaterialNavbar>
+
+
+
+                <Dialog open={open} size="xs" handler={handleOpen}>
+        <div className="flex items-center justify-between">
+          <DialogHeader className="flex flex-col items-start">
+            {" "}
+            <Typography className="mb-1" variant="h4">
+              Send Feedback{" "}
+            </Typography>
+          </DialogHeader>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="mr-3 h-5 w-5"
+            onClick={handleOpen}
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <DialogBody>
+          <Typography className="mb-10 -mt-7 " color="gray" variant="lead">
+            Write the message and then click button.
+          </Typography>
+          <div className="grid gap-6">
+            <Typography className="-mb-1" color="blue-gray" variant="h6">
+              Message
+            </Typography>
+            
+            <Textarea label="Message" />
+          </div>
+        </DialogBody>
+        <DialogFooter className="space-x-2">
+          <Button variant="text" color="gray" onClick={handleOpen}>
+            cancel
+          </Button>
+          <Button variant="gradient" color="blue" onClick={handleOpen}>
+            send
+          </Button>
+        </DialogFooter>
+      </Dialog>
 
             </div>
 
