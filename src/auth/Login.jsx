bad37793acb874a9056330fa5bc2de46109cc958
Akshh2i3 <akshaysethiya23@gmail.com>
2024-07-28 12:55:03 +0530

    changed login and sign up page

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -4,7 +4,7 @@ import { app } from '../Firebase'
 import { useNavigate, Link, useLocation } from 'react-router-dom'
 import axios from 'axios'
 
-import SyncLoader from "react-spinners/SyncLoader";
+import SsyncLoader from "react-spinners/SyncLoader";
 
 import { Toaster, toast } from 'sonner'
 
@@ -146,22 +146,21 @@ const Login = () => {
       <Toaster richColors position="bottom-center" />
 
       {/* source:https://codepen.io/owaiswiz/pen/jOPvEPB */}
-      <div className="max-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className="h-screen items-center bg-gray-200 text-gray-900 flex justify-center">
 
-        <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+        <div className="max-w-screen-xl m-0 sm:m-10 sm:rounded-lg flex justify-center flex-1 h-fit rounded-xl overflow-hidden shadow-lg">
+          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 my-auto bg-gray-50">
             <div>
               {/* <img
             src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
             className="w-32 mx-auto"
           /> */}
-              <p className='text-center'>*company logo*</p>
             </div>
             <div className="mt-6 flex flex-col items-center">
-              <h1 className="text-2xl xl:text-3xl font-extrabold">Log In</h1>
+              <h1 className="text-2xl xl:text-3xl font-bold">Log In</h1>
               <div className="w-full flex-1 mt-8">
                 <div className="flex flex-col items-center">
-                  <button onClick={handleLoginWithGoogle} className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                  {/* <button onClick={handleLoginWithGoogle} className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
                     <div className="bg-white p-2 rounded-full">
                       <svg className="w-4" viewBox="0 0 533.5 544.3">
                         <path
@@ -187,14 +186,14 @@ const Login = () => {
                         isLogInGoogleLoading ? <SyncLoader color="#fff" size={8} /> : "Log in with Google"
                       }
                     </span>
-                  </button>
+                  </button> */}
 
                 </div>
-                <div className="my-6 border-b text-center">
+                {/* <div className="my-6 border-b text-center">
                   <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
                     Or Log in with e-mail
                   </div>
-                </div>
+                </div> */}
                 <div className="mx-auto max-w-xs">
                   <input
                     className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
@@ -251,12 +250,12 @@ const Login = () => {
               </div>
             </div>
           </div>
-          <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+          <div className="flex-1 bg-white text-center hidden lg:flex">
             <div
               className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
               style={{
                 backgroundImage:
-                  'url("https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg")'
+                  'url("/IMC2.png")'
               }}
             ></div>
           </div>
